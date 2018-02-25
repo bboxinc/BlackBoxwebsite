fbb52f36de445c84b714a1dad851fb68f2129a6d
djbabai <djbabai.debjyoti@gmail.com>
2018-02-25 08:01:42 +0000

    fixed web.py

--- web.py
+++ web.py
@@ -11,8 +11,5 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-  app.run()
-=======
   app.run(host='192.168.1.3',port=5050)
->>>>>>> 8b815f899f0e6dc7e80fd27393262b1621052599
+
