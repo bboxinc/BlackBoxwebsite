0414e0edcd20d0799db7b66f1839c908fadbb7bd
djbabai <djbabai.debjyoti@gmail.com>
2018-02-25 13:18:08 +0530

    added login form

--- web.py
+++ web.py
@@ -3,9 +3,12 @@ from flask import render_template
 app = flask.Flask(__name__)
 
 @app.route('/')
-
 def page():
   return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
-  app.run(port=8080)
+  app.run()
